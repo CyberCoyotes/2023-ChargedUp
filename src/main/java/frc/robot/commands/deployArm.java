12e29c40a93cbf4fe624af9f5a6327230a15cf2f
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-02-01 15:18:51 -0500

    renamed

--- src/main/java/frc/robot/commands/deployArm.java
+++ src/main/java/frc/robot/commands/deployArm.java
@@ -1,5 +1,5 @@
 package frc.robot.commands;
 
-public class setWristPosition {
+public class deployArm {
     
 }
---
+++ src/main/java/frc/robot/commands/setArmPosition.java
@@ -0,0 +1,5 @@
+package frc.robot.commands;
+
+public class setArmPosition {
+    // Set arm to starting position and partially deployed positions
+}
