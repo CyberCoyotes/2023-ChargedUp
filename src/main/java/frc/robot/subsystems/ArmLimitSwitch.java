310723ec1e94b593c6ab087345d4489248ac6763
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-02-22 14:35:52 -0500

    limit switch start, non functional for now

--- src/main/java/frc/robot/subsystems/ArmLimitSwitch.java
+++ src/main/java/frc/robot/subsystems/ArmLimitSwitch.java
@@ -9,6 +9,23 @@ public boolean getLimitSwitchState() {
 }
 
 /*
+ * Encoder encoder = new Encoder(0, 1);
+Spark spark = new Spark(0);
+// Limit switch on DIO 2
+DigitalInput limit = new DigitalInput(2);
+public void autonomousPeriodic() {
+// Runs the motor backwards at half speed until the limit switch is pressed
+// then turn off the motor and reset the encoder
+if(!limit.get()) {
+spark.set(-0.5);
+} else {
+spark.set(0);
+encoder.reset();
+}
+}
+ */
+
+/*
  * Magnetic Limit Switch
  * https://www.revrobotics.com/rev-31-1462/
  */
