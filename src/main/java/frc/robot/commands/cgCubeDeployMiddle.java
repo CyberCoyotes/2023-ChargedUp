7490c0b132988997e51ad20fa9d79ac62066c1f7
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-03-02 09:56:40 -0500

    pseudo auton for cube to middle and clear the zone

--- src/main/java/frc/robot/commands/cgCubeDeployMiddle.java
+++ src/main/java/frc/robot/commands/cgCubeDeployMiddle.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.ArmExtensionSubsystem;
@@ -9,6 +8,8 @@ import frc.robot.subsystems.ClawSubsystem;
 
 public class cgCubeDeployMiddle extends SequentialCommandGroup {
 
+
+
     public cgCubeDeployMiddle(ArmSubsystem m_ArmSub, ArmExtensionSubsystem m_ArmExtSub, ClawSubsystem m_ClawSub) {
 
         // FIXME Needs testing and some actual numbers
@@ -16,13 +17,14 @@ public class cgCubeDeployMiddle extends SequentialCommandGroup {
                 new SetClawClose2(m_ClawSub),
                 new RotateArmDeploy(m_ArmSub), // Rotate the arm to approximately 90 degrees. Determine experimentally
                 new SetArmExtend(m_ArmExtSub), // Extend the arm 6000 ticks?. Determine experimentally
-                new RotateArmDeploy(m_ArmSub), // Rotate the arm to approximately 90 degrees (again to compensate for sag) . Determine experimentally
+                new RotateArmDeploy(m_ArmSub), // Rotate the arm to approximately 90 degrees (again to compensate for
+                                               // sag) . Determine experimentally
                 new SetClawOpen2(m_ClawSub), // open the claw
                 new WaitCommand(.15), // short pause
                 new SetClawClose2(m_ClawSub), // open the claw
                 new SetArmIn(m_ArmExtSub), // Retract the arm same as extension ticks
                 new RotateArmIntake(m_ArmSub) // Rotate arm back to intake position 0 degrees or shorter 20 degrees
-                // move on to drive phase
+        // In auton, this would move on to drive phase
 
         ); // end of commands
     }
