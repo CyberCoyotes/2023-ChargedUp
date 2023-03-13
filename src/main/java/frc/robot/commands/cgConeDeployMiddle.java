be581affd3949282532087c980bcfe64dfb90089
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-03-13 17:51:11 -0400

    still

--- src/main/java/frc/robot/commands/cgConeDeployMiddle.java
+++ src/main/java/frc/robot/commands/cgConeDeployMiddle.java
@@ -1,7 +1,18 @@
+/*--------------------------------------------------------
+*
+* Deploy Cone to Mid Level
+* Wrist Pos = 72250;
+* Arm Rot = -90;
+* Arm Extension = full;
+* 
+*--------------------------------------------------------*/
+
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 
 public class cgConeDeployMiddle extends SequentialCommandGroup{
 }
