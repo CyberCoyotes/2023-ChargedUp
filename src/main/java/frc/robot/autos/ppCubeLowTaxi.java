3365a71f90d32f02ecb807ff2449f492048b70b8
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-04-01 20:52:11 -0400

    Create ppCubeLowTaxi.java

---
+++ src/main/java/frc/robot/autos/ppCubeLowTaxi.java
@@ -0,0 +1,51 @@
+/* 
+ * 
+ * Original "CubeLowTaxi.java"
+ * 
+*/
+package frc.robot.autos;
+
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.TeleopSwerve;
+import frc.robot.RobotContainer;
+import frc.robot.commands.CubeLow;
+import frc.robot.subsystems.ArmExtensionSubsystem;
+import frc.robot.subsystems.ArmRotationSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.ArmWristSubsystem;
+
+
+public class ppCubeLowTaxi extends SequentialCommandGroup
+{
+    private ArmRotationSubsystem m_arm;
+    private ArmExtensionSubsystem m_extend;
+    private IntakeSubsystem m_intake;
+    private ArmWristSubsystem m_wrist;
+
+
+    public ppCubeLowTaxi(ArmExtensionSubsystem extend, ArmRotationSubsystem arm, IntakeSubsystem intake, ArmWristSubsystem wrist, BooleanSupplier robotCentric) {
+
+            this.m_arm = arm; 
+            this.m_extend = extend; 
+            this.m_intake = intake; 
+
+        List<PathPlannerTrajectory> pptList = PathPlanner.loadPathGroup("Taxi4meters", new PathConstraints(4, 3));
+    
+            addRequirements();
+
+        addCommands(
+            new CubeLow(m_arm, m_extend, m_wrist, m_intake ).withTimeout(7),
+            RobotContainer.buildAuton(pptList)
+
+        );
+    }
+}
