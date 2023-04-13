49f5b48beefc1a3c6702aad657ff667aebf1bf9a
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-04-12 21:24:36 -0400

    WIP another attempt at conditional

--- src/main/java/frc/robot/commands/ExtendToFloor_version2.java
+++ src/main/java/frc/robot/commands/ExtendToFloor_version2.java
@@ -13,17 +13,17 @@ import frc.robot.Constants.Arm;
 import frc.robot.subsystems.ArmExtensionSubsystem;
 
 
-public class ArmExtendToFloor extends CommandBase {
+public class ExtendToFloor_version2 extends CommandBase {
 
     private final ArmExtensionSubsystem m_armExtensionSubsystem;
 
-    
-    public ArmExtendToFloor(ArmExtensionSubsystem subsystem) {
+
+    private Boolean position = false;
+
+    public ExtendToFloor_version2(ArmExtensionSubsystem subsystem) {
 
      m_armExtensionSubsystem = subsystem;
      
-
-    
      addRequirements(m_armExtensionSubsystem);
     
     }
@@ -35,9 +35,15 @@ public class ArmExtendToFloor extends CommandBase {
     
     @Override
     public void execute() {
-        // TODO VanScoyoc shot in the dark
-        m_armExtensionSubsystem.ReadExtension(); // Reads the current extension encoder position
+        // TODO VanScoyoc shot in the dark #2
+        // m_armExtensionSubsystem.ReadExtension(); // Reads the current extension encoder position
         m_armExtensionSubsystem.extendToFloorCube(); // Extends to a set encoder value. Testing with a shorter value and slower speed
+        
+        // if ((m_armExtensionSubsystem.ReadExtension()  >= (Arm.EXTENSION_FLOOR_POS*0.95) && (m_armExtensionSubsystem.ReadExtension() <= (Arm.EXTENSION_FLOOR_POS*1.05)))) { 
+        if ((m_armExtensionSubsystem.ReadExtension()  <= -700) && (m_armExtensionSubsystem.ReadExtension() >= -1500)) { 
+            // FIXME Should be a constant around 9500 but using short extension for testing
+            position = true;
+        }
     }
 
 
@@ -45,12 +51,7 @@ public class ArmExtendToFloor extends CommandBase {
     public boolean isFinished() {
         // TODO VanScoyoc shot in the dark
         // Attempting to use a conditional check against encoder value vs desired value w/o using fancy PID
-        if (m_armExtensionSubsystem.ReadExtension()  <= Arm.EXTENSION_FLOOR_POS) { // FIXME Should be a constant around 9500 but using short extension for testing
-            return true;
-        } else {
-            return true; // Runs without stopping
-        }
-    
+            return position;    
     }
 
 }
